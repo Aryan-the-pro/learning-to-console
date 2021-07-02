cf5886270d0a434573c5c9a5d835c8d21b763abd
Aryan-the-pro <86834170+Aryan-the-pro@users.noreply.github.com>
2021-07-02 17:10:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log("this is a console project") ; 
+var first_name ="Aryan" ;  
+console.log(first_name) ;
+var last_name ="Khatri" ;  
+console.log(last_name) ; 
+var full_name = first_name.concat(last_name) ; 
+console.log(full_name) ;
+var numb_1 =66 ; 
+console.log(numb_1) ; 
+var numb_2 =99;
+console.log(numb_2) ;
+var add =numb_1 + numb_2 ;
+console.log(add)
+var sub = numb_2-numb_1 ; 
+console.log(sub) ; 
+var mul = numb_1*numb_2 ;  
+console.log(mul) ; 
+var div = numb_2 / numb_1 ; 
+console.log(div); 
+
